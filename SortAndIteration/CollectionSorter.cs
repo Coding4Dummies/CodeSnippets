9dceb347f9294d9959e9291d75074d31589392aa
Anton Dorofeyev <dorofeyev@gmail.com>
2017-10-16 23:08:44 +0300

    sorting examples

---
+++ SortAndIteration/CollectionSorter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace SortAndIteration
+{
+    class CollectionSorter
+    {
+        public T[] SortTwoItemTypesArrayWithHelper<T>(T[] inputArray, T leftItem) where T : IComparable
+        {
+            var resultArray = new T[inputArray.Length];
+
+            var leftIndex = 0;
+            var rightIndex = resultArray.Length - 1;
+
+            // Single iteration over the collection:
+            foreach (T item in inputArray)
+            {
+                if (item.CompareTo(leftItem) == 1)
+                {
+                    resultArray[rightIndex] = item;
+                    rightIndex--;
+                    continue;
+                }
+                resultArray[leftIndex] = item;
+                leftIndex++;
+            }
+            return resultArray;
+        }
+
+        public T[] SortTwoItemTypesArrayNoHelper<T>(T[] inputArray, T leftItem) where T : IComparable
+        {
+            var leftIndex = 0;
+            var rightIndex = inputArray.Length - 1;
+
+            while (leftIndex < rightIndex)
+            {
+                if (inputArray[leftIndex].CompareTo(leftItem) == 1)
+                {
+                    while (inputArray[rightIndex].CompareTo(leftItem) == 1)
+                    {
+                        rightIndex--;
+
+                        if (leftIndex > rightIndex)
+                            return inputArray;
+                    }
+                    SwapValues(leftIndex, rightIndex, inputArray);
+                }
+                leftIndex++;
+            }
+            return inputArray;
+        }        
+
+        public T[] SortThreeItemTypesArray<T>(T[] inputArray, T middleItem) where T : IComparable
+        {
+            var leftIndex = 0;
+            var rightIndex = inputArray.Length - 1;
+            var iterator = 0;
+
+            while (iterator <= rightIndex)
+            {
+                if (inputArray[iterator].CompareTo(middleItem) < 0)
+                {
+                    SwapValues(iterator, leftIndex, inputArray);
+                    leftIndex++;
+                    iterator ++;
+                    continue;
+                }
+
+                if (inputArray[iterator].CompareTo(middleItem) > 0)
+                {
+                    SwapValues(iterator, rightIndex, inputArray);
+                    rightIndex--;
+                    continue;
+                }
+
+                iterator ++;
+            }
+
+            return inputArray;
+        }
+
+        private void SwapValues<T>(int firstIndex, int secondIndex, T[] array)
+        {
+            T tempStorage = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = tempStorage;
+        }
+    }
+}
+
---
+++ SortAndIteration/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace SortAndIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestSortTwoItemTypesArrayWithHelper();
+
+            TestSortTwoItemTypesArrayNoHelper();
+
+            TestSortThreeItemTypesArray();
+        }
+
+        private static void TestSortTwoItemTypesArrayWithHelper()
+        {
+            var unsortedArray = new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' };
+
+            var arraySorter = new CollectionSorter();
+
+            System.Console.WriteLine("Before Sort:");
+            PrintCollection(unsortedArray);
+
+            var sortedArray = arraySorter.SortTwoItemTypesArrayWithHelper<char>(unsortedArray, 'a');
+
+            System.Console.WriteLine("After Sort:");
+            PrintCollection(sortedArray);
+        }
+
+        private static void TestSortTwoItemTypesArrayNoHelper()
+        {
+            var unsortedArray = new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' };
+
+            var arraySorter = new CollectionSorter();
+
+            System.Console.WriteLine("Before Sort:");
+            PrintCollection(unsortedArray);
+
+            var sortedArray = arraySorter.SortTwoItemTypesArrayNoHelper<char>(unsortedArray, 'a');
+
+            System.Console.WriteLine("After Sort:");
+            PrintCollection(sortedArray);
+        }
+        private static void TestSortThreeItemTypesArray()
+        {
+            var unsortedArray = new[] { 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a', 'b' };
+
+            var arraySorter = new CollectionSorter();
+
+            unsortedArray = new[] { 'a', 'c', 'b', 'b', 'c', 'c', 'c', 'a', 'a', 'a', 'c', 'b', 'a', 'c', 'b', 'a', 'c', 'b', 'a', 'c' };
+
+            System.Console.WriteLine("Before Sort:");
+            PrintCollection(unsortedArray);
+
+            var sortedArray = arraySorter.SortThreeItemTypesArray<char>(unsortedArray, 'b');
+
+            System.Console.WriteLine("After Sort:");
+            PrintCollection(sortedArray);
+        }
+
+        static void PrintCollection(IEnumerable collection)
+        {
+            foreach (var item in collection)
+            {
+                if (item is null)
+                {
+                    Console.Write("null ");
+                    continue;
+                }
+                Console.Write(item.ToString() + " ");
+            }
+            Console.Write('\n');
+        }
+    }
+}
